8bddf1e7c793c2bf66e15cc17fa09a84cdadaa35
Tine <111304307+TineMSPetersen@users.noreply.github.com>
2022-09-03 17:07:54 +0200

    Create function operate
    Created function that will call one of the calculator functions as well as the two numbers and return the result

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ function multiply (a, b) {
 function divide (a, b) {
     return a / b;
 }
+
+function operate(operator, num1, num2) {
+    return operator(num1, num2);
+}
+
+function operate(operator, num1, num2) {
+    return operator(num1, num2);
+}
